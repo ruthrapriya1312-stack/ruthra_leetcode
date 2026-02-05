7cca95a674a67d08ebf68c52e5168c1d8a8b3cbb
ruthrapriya1312-stack <ruthrapriya1312@gmail.com>
2026-02-05 10:03:09 +0530

    Create 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray

---
+++ 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray/2624-DifferenceBetweenElementSumAndDigitSumOfAnArray.java
@@ -0,0 +1,14 @@
+// Last updated: 05/02/2026, 10:03:08
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int elementSum = 0, digitSum = 0;
+        for (int n : nums) {
+            elementSum += n;
+            while (n > 0) {
+                digitSum += n % 10;
+                n /= 10;
+            }
+        }
+        return Math.abs(elementSum - digitSum);
+    }
+}
